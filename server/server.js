3f0ef42d730b57c1035e6521e61b0ef0ac7ca7f6
Ankush Ladani <astarladani@gmail.com>
2022-11-04 11:20:45 +0530

    Fetching All Transaction Done

--- server/server.js
+++ server/server.js
@@ -1,25 +1,40 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import Transaction from "./models/Transaction.js";
+import dotenv from "dotenv";
 dotenv.config();
 
-const connect = async () => {
-  await mongoose.connect(process.env.MONGO_URL);
-};
-connect();
-console.log("CONNECTION SUCCESSFUL");
-
-// import express from "express";
-// ? We can import express from 'express' but for that we have to set "type" : "module" in package.json
-const app = express();
-
-app.use(cors());
-
 const PORT = 4000;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+await mongoose.connect(process.env.MONGO_URL);
+console.log("MongoDB connection is successful");
 
 app.get("/", (req, res) => {
-  res.send("Good Morning");
+  res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log(`Server is Running at ${PORT} PORT`));
+app.get("/transaction", async (req, res) => {
+  // During sorting if we provide -1 then latest will come first and if 1 then latest will come last
+  const transaction = await Transaction.find({}).sort({ createdAt: 1 });
+  res.json(transaction);
+});
+
+app.post("/transaction", async (req, res) => {
+  const { amount, description, date } = req.body;
+  const transaction = new Transaction({
+    amount,
+    description,
+    date,
+  });
+  await transaction.save();
+  res.json({ message: "Success" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:4000");
+});
